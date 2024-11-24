7a78b8fc713ab77190a4bc51c2a9ef7b3f2b2527
SofiaSenkiv27 <senkiv.pn@ucu.udu.ua>
2024-11-24 18:20:48 +0200

    changed checkstyle

--- src/main/java/ua/ucu/edu/ua/task2/MailSender.java
+++ src/main/java/ua/ucu/edu/ua/task2/MailSender.java
@@ -4,7 +4,7 @@ public class MailSender {
     public void sendMail(MailInfo mailInfo) {
         String message = mailInfo.generate(); 
         System.out.println("Sending mail to " 
-        + mailInfo.getClient().getName() +
-        " (ID: " + mailInfo.getClient().getId() + "): " + message);
+        + mailInfo.getClient().getName() 
+        + " (ID: " + mailInfo.getClient().getId() + "): " + message);
     }
 }
